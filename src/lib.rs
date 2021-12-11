6554e801f6c2ae426e8a582d9f40e3fea47e38c6
Marvin Mednick <mmednick@gmail.com>
2021-12-10 16:47:51 -0800

    updating before re-org

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,97 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::collections::{Vector};
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    serde::{Deserialize, Serialize},
+    AccountId, PanicOnDefault
+};
+use near_sdk::{env, near_bindgen};
+
+/*
+use near_sdk::collections::{LookupMap, UnorderedSet};
+use near_sdk::{
+    borsh::{self, BorshDeserialize, BorshSerialize},
+    log,
+    serde::{Deserialize, Serialize},
+    AccountId, PanicOnDefault, Promise,
+};
+use near_sdk::{env, near_bindgen};
+
+// 5 Ⓝ in yoctoNEAR
+const PRIZE_AMOUNT: u128 = 5_000_000_000_000_000_000_000_000;
+
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub enum AnswerDirection {
+    Across,
+    Down,
+}
+
+/// The origin (0,0) starts at the top left side of the square
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct CoordinatePair {
+    x: u8,
+    y: u8,
+}
+
+*/
+
+/// The origin (0,0) starts at the top left side of the square
+#[derive(BorshDeserialize, BorshSerialize, Deserialize, Serialize, Debug)]
+#[serde(crate = "near_sdk::serde")]
+pub struct LogEntry {
+    timestamp: String,
+    account: AccountId,
+    name: String,
+    message: String,
+}
+
 
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize,PanicOnDefault)]
 pub struct Contract {
-    // SETUP CONTRACT STATE
+    owner_id:  AccountId,
+    mylog:  Vector<LogEntry>
+    
 }
 
 #[near_bindgen]
 impl Contract {
-    // ADD CONTRACT METHODS HERE
+
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        Self {
+            owner_id,
+            mylog: Vector::new(b"c"),        
+        }
+    }
+
+    pub fn add_entry (&mut self, user_id: &AccountId, timestamp: String, name: String, message: String) {
+        let new_entry = LogEntry {
+            timestamp : timestamp.clone(),
+            account : user_id.clone(),
+            name,
+            message   
+        };
+        self.mylog.push(&new_entry);
+        env::log_str("Entry Added!");
+    }
+
+    pub fn num_entries(&self) -> u64 {
+        self.mylog.len()
+    }
+
+    pub fn list_entries(&self) -> String {
+        let mut count = 0;
+        let mut result = String::new();
+        for entry in self.mylog.iter() {
+            count = count + 1;
+            let line = format!("Entry #{} TS: {} NAME: {} ACCT: {} MSG: {}",count, entry.timestamp, entry.name, entry.account, entry.message);
+            result += &line;
+            println!("{}",&result);
+        }
+        result
+    }
 }
 
 /*
@@ -34,5 +116,26 @@ mod tests {
         builder
     }
 
-    // TESTS HERE
+     #[test]
+    fn check_add_entry() {
+        let account = AccountId::new_unchecked("mmednick.testnet".to_string());
+        let context = get_context(account);
+        testing_env!(context.build());
+
+        let account = AccountId::new_unchecked("mmednick.testnet".to_string());
+        let send_account = AccountId::new_unchecked("mmednick.testnet".to_string());
+        let mut contract = Contract::new(account);
+        contract.add_entry(&send_account,"dateAndTime".to_string(),"NameofSam".to_string(),"My Message is".to_string());
+        
+        assert_eq!(get_logs(), ["Entry Added!"], "Exec.");
+
+        assert_eq!(contract.num_entries(),1);
+
+        contract.add_entry(&send_account,"dateAndTime".to_string(),"My Name is".to_string(),"My Message is".to_string());
+        assert_eq!(contract.num_entries(),2);
+
+        contract.list_entries()
+
+    }
+
 }
