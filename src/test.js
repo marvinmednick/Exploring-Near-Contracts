f36b5a666fe867ad902ece8f74004fa52b16e60e
Marvin Mednick <mmednick@gmail.com>
2022-01-13 07:33:39 -0800

    Added test for checking get_last

--- src/test.js
+++ src/test.js
@@ -1,7 +1,10 @@
 describe('Token', function () {
   let near;
   let contract;
+  let subcontract;
   let accountId;
+  let main_init = false;
+  let sub_init = false;
 
   beforeAll(async function () {
     debugger;
@@ -11,7 +14,7 @@ describe('Token', function () {
     subAccountId = nearConfig1.contractName;
     contract = await near.loadContract(nearConfig.contractName, {
       viewMethods: ['num_entries'],
-      changeMethods: ['new','add_entry', 'reset_log'],
+      changeMethods: ['new','add_entry', 'reset_log', 'get_last'],
       sender: accountId
     });
     subcontract = await near.loadContract(nearConfig1.contractName, {
@@ -21,31 +24,34 @@ describe('Token', function () {
     });
   });
 
-  describe('entries', function () {
+  
+  describe('init', function () {
+
+	var counter;
+	var startCounter;
 
     it('main contract can be initialized', async function() {
       await contract.new({"args" : {}});
-      const endCounter = await contract.num_entries({"args": {}});
-      expect(endCounter).toEqual(0);
+      const counter = await contract.num_entries({"args": {}});
+      expect(counter).toEqual(0);
     });
     it('subcontract can be initialized', async function() {
       await subcontract.new({"args" : {"log_contract" : accountId}});
-      const endCounter = await subcontract.indirect_num_entries({"args" : {}});
-      expect(endCounter).toEqual(0);
+      const counter = await subcontract.indirect_num_entries({"args" : {}});
+      expect(counter).toEqual(0);
     },10000);
+    it('count can be accessed', async function() {
+      startCounter = await contract.num_entries({"args": {}});
+      expect(startCounter).toEqual(0);
+    });
+    it('main contract can add entries', async function () {
+      await contract.add_entry({"args" : { "timestamp" : "Time1","name" : "My Name","message": "My Message"}});
+      counter = await contract.num_entries({"args": {}});
+      expect(counter).toEqual(startCounter + 1);
+    });
     it('sub account config is can be read', async function() {
       const cfg_contract = await subcontract.info({"args" : {}});
       expect(cfg_contract).toEqual(accountId);
-    });
-    it('count can be accessed', async function() {
-      const endCounter = await contract.num_entries({"args": {}});
-      expect(endCounter).toEqual(0);
-    });
-    it('main contract can add entries', async function () {
-      const startCounter = await contract.num_entries({"args": {}});
-      await contract.add_entry({"args" : { "timestamp" : "Time1","name" : "My Name","message": "My Message"}});
-      const endCounter = await contract.num_entries({"args": {}});
-      expect(endCounter).toEqual(startCounter + 1);
     });
     it('sub contract can add entries', async function () {
       const startCounter = await contract.num_entries({"args": {}});
@@ -53,6 +59,19 @@ describe('Token', function () {
       const endCounter = await subcontract.indirect_num_entries({"args": {}});
       expect(endCounter).toEqual(startCounter + 1);
     },15000);
+    it('get last data is valid', async function () {
+      await subcontract.indirect_add_entry({"args" : { "timestamp" : "Time3","name" : "Luke","message": "Your father", "transfer_amount" : "0"}});
+      var last_data = await contract.get_last({"args": {}});
+      var last_entry = JSON.parse(last_data);
+	  expect(last_entry.name).toEqual("indirect Luke");
+	  expect(last_entry.message).toEqual("indirect Your father");
+
+      await contract.add_entry({"args" : { "timestamp" : "Time1","name" : "R2D2","message": "Beep, boop"}});
+      last_data = await contract.get_last({"args": {}});
+      last_entry = JSON.parse(last_data);
+	  expect(last_entry.name).toEqual("R2D2");
+	  expect(last_entry.message).toEqual("Beep, boop");
+    },12000);
     it('can be reset', async function () {
       await contract.reset_log({"args": {}});
       const endCounter = await contract.num_entries({"args": {}});
