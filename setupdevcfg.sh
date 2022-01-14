b42fad8b1e0e08760b768e0a3a4af8624429e590
Marvin Mednick <mmednick@gmail.com>
2022-01-13 21:08:58 -0800

    Fixed init, env and deployment commands

--- setupdevcfg.sh
+++ setupdevcfg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "export CONTRACT_NAME=`cat contract/neardev/dev-account`" > devconfig
-echo "export SUBCONTRACT_NAME=`cat contract2/neardev/dev-account`" >> devconfig
+echo "export CONTRACT_NAME=`cat contracts/marvfirst_main/neardev/dev-account`" > devconfig
+echo "export SUBCONTRACT_NAME=`cat contracts/marvfirst_sub/neardev/dev-account`" >> devconfig
 echo "exports stored in \"devconfig\" file.  Type \"source devconfig\" to setup environment"
